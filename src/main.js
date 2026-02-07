bdf7b9c682c444d5ff231352ea985f4a033d59bb
TotoCode <totocodefr@duck.com>
2026-02-07 20:32:23 +0100

    dynamic app loading (without the junk from devdash)
    devdash: https://github.com/DevdashApp/Devdash

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import express from 'express';
+import { loadApps } from './utility/loader.js';
 
 const app = express();
 
 app.use(express.json());
 
+// main and public assets
 app.use('/app', express.static('src/app'));
 app.use('/', express.static('src/public'));
 
-app.listen(3000, () => {
-    console.log('Front is running on port 3000');
+// load apps
+await loadApps(app);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Front is running on port ${PORT}`);
 });
