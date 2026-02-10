7c9329dba9515eb27f92090e353a26a1ce95c732
TotoCode <totocodefr@duck.com>
2026-02-10 18:17:23 +0000

    add supabase and doppler

--- src/utility/supabase.js
+++ src/utility/supabase.js
@@ -5,12 +5,12 @@ let supabase = null;
 export function getSupabaseClient() {
     if (!supabase) {
         const supabaseUrl = process.env.SUPABASE_URL;
-        const supabaseKey = process.env.SUPABASE_KEY;
-
+        const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_ANON_KEY;
+        
         if (!supabaseUrl || !supabaseKey) {
             throw new Error('Supabase URL and Key must be set in environment variables');
         }
-
+        
         supabase = createClient(supabaseUrl, supabaseKey, {
             // optional config:
             auth: {
